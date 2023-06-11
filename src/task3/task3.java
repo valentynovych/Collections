4f75f96a9dc9960bc06c917d7d822e7f3f639a57
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2023-06-11 14:02:55 +0300

    task 3

---
+++ src/task3/task3.java
@@ -0,0 +1,51 @@
+package task3;
+
+import java.util.*;
+
+public class task3 {
+    private static Set<String> numbersList = new HashSet<>();
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String stringIn;
+
+        do {
+            System.out.println("Please enter vehicle number and insert position \n" +
+                    "\tor List for print all elements\n" +
+                    "\tor Stop for exit ");
+            stringIn = scanner.next();
+
+            switch (stringIn) {
+                case "Stop":
+                    System.exit(0);
+                    break;
+                case "List":
+                    printList(numbersList);
+                    break;
+                default:
+                    addElementToPosition(stringIn);
+
+            }
+        } while (true);
+    }
+
+    private static void addElementToPosition(String number) {
+
+        if (numbersList.add(number)) {
+            System.out.println("Vehicle number: " + number + " added to collection");
+        } else {
+            System.out.println("This number already exist");
+        }
+    }
+
+
+    private static void printList(Set<String> numbersList) {
+        System.out.println("All vehicle number in list: ");
+        for (String number : numbersList) {
+
+            System.out.println(" " + number);
+        }
+    }
+}

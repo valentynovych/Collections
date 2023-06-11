941521088891aaa1c2e032ba7115694181a50322
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2023-06-11 11:41:11 +0300

    task 1

--- src/task1/task1.java
+++ src/task1/task1.java
@@ -7,22 +7,30 @@ import java.util.Scanner;
 public class task1 {
     public static void main(String[] args) {
         List<String> numbersList = new ArrayList<>();
-
         Scanner scanner = new Scanner(System.in);
 
         String number;
+
         do {
-            System.out.println("Please enter car number or List or Stop: ");
+            System.out.println("Please enter vehicle number or List or Stop: ");
             number = scanner.next();
             switch (number) {
                 case "Stop":
                     System.exit(0);
                     break;
                 case "List":
-                    System.out.println(numbersList);
+                    printList(numbersList);
                 default:
                     numbersList.add(number);
+                    System.out.println("Vehicle number: " + number + " added to collection");
             }
         } while (true);
+    }
+
+    private static void printList(List<String> numbersList) {
+        System.out.println("All vehicle number in list: ");
+        for (String number : numbersList) {
+            System.out.println((numbersList.indexOf(number) + 1) + ") " + number);
+        }
     }
 }

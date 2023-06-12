9ec1f9530fe3ed47891ba2a8f551801715460e00
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2023-06-12 12:37:33 +0300

    refactor interface

--- src/task5/SpeedTest.java
+++ src/task5/SpeedTest.java
@@ -1,10 +1,17 @@
 package task5;
 
 public interface SpeedTest {
+    void fillArray();
+
     void addLast();
+
     void addFirstPart();
+
     void getLast();
+
     void getFirstPart();
+
     void removeLast();
+
     void removeFirstPart();
 }
